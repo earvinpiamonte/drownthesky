cb816647bcb8f5966a4617c14ff6967084aa0073
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-12-04 01:47:57 +0800

    Update home page

--- pages/index.js
+++ pages/index.js
@@ -31,29 +31,29 @@ const Home = () => {
       </Head>
       <section className="h-screen">
         <div className="grid grid-cols-3 h-full">
-          <div className="md:col-span-2 col-span-3 md:h-auto h-screen">
-            <div className="relative h-full bg-gray-100">
+          <div className="md:col-span-2 col-span-3">
+            <div className="relative h-screen bg-gray-100">
               {data.hdurl && (
                 <Image
                   src={data.hdurl}
                   layout="fill"
                   objectFit="cover"
                   quality={100}
+                  loading="eager"
+                  priority={true}
                 />
               )}
-              <div className="absolute bg-black bg-opacity-30 md:bottom-0 md:top-auto top-0 right-0 px-4 py-1">
+              <div className="absolute bg-black bg-opacity-30 bottom-0 right-0 px-4 py-1">
                 <h2 className="text-white text-xs">
-                  &copy; {data.copyright || `NASA`}
+                  &copy; {data.copyright || `NASA`}{" "}
+                  <time dateTime={data.date}>{data.date}</time>
                 </h2>
               </div>
             </div>
           </div>
-          <div className="md:col-span-1 col-span-3 md:order-first md:mt-0 -mt-32 md:overflow-y-auto">
+          <div className="md:col-span-1 col-span-3 md:order-first md:overflow-y-auto">
             <div className="py-10 px-4 relative bg-white">
-              <h1 className="font-bold uppercase mb-1">{data.title}</h1>
-              <p className="mb-8">
-                <time dateTime={data.date}>{data.date}</time>
-              </p>
+              <h1 className="text-2xl mb-8">{data.title}</h1>
               <p className="mb-4">{data.explanation}</p>
             </div>
           </div>
